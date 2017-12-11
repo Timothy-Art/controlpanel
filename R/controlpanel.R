f246e1415126fdf5909256130514275477754001
timothy-art <timothy@rnfc.org>
2017-12-11 10:01:18 -0500

    updated styling, fixed documentation

--- R/controlpanel.R
+++ R/controlpanel.R
@@ -1,3 +1,5 @@
+require(rjson)
+
 #' controlpanel
 #'
 #' Creates a control panel for adjusting factors and weights
@@ -5,14 +7,17 @@
 #' @param opts a list of options to give the control panel
 #' @param factorList a character vector of factors that will be made available in the Add Factor dropdown menu
 #' @param multiSelect TRUE/FALSE if you want the user to be able to select more than one panel at a time
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param elementId Use an explicit element ID for the widget (rather than an automatically generated one).
+#'   Useful if you have other JavaScript that needs to explicitly discover and interact with a specific widget instance.
 #' @return a controlpanel html widget
 #' @import htmlwidgets
 #' @import rjson
-#' 
+#'
 #' @export
 controlpanel <- function(opts, factorList=c(), multiSelect=FALSE, width = NULL, height = NULL, elementId = NULL) {
-  require(rjson)
-
   data <- toJSON(opts)
 
   x <- list(
@@ -45,10 +50,11 @@ controlpanelOutput <- function(outputId, width = '100%', height = 'auto'){
 }
 
 #' Shiny render for controlpanelOutput
-#' 
+#'
 #' @param expr An expression that generates a controlpanel
 #' @param env The environment in which to evaluate \code{expr}.
-#'   
+#' @param quoted Is expr a quoted expression (with quote())? This is useful if you want to save an expression in a variable.
+#'
 #' @export
 renderControlpanel <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
